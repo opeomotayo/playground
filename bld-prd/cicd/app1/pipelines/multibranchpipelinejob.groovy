21608f331a2f2770ec8554d9893223efb5cb7cd0
Ope Omotayo <opeomotayo@gmail.com>
2022-06-21 22:53:21 +0000

    creating new pipeline

--- bld-prd/cicd/app1/pipelines/multibranchpipelinejob.groovy
+++ bld-prd/cicd/app1/pipelines/multibranchpipelinejob.groovy
@@ -1,15 +1,41 @@
-multibranchPipelineJob('multibranch-pipeline-job') {
-    branchSources {
-        git {
-            id('github-key') // IMPORTANT: use a constant and unique identifier
-            remote('git@github.com:opeomotayo/building-a-multibranch-pipeline-project.git')
-            credentialsId('github-key')
-            includes('JENKINS-*')
-        }
+multibranchPipelineJob('building-a-multibranch-pipeline-project') {
+  branchSources {
+    github {
+      id('building-a-multibranch-pipeline-project')
+      repoOwner('opeomotayo')
+      repository('building-a-multibranch-pipeline-project')
+      scanCredentialsId('github-key')
     }
-    orphanedItemStrategy {
-        discardOldItems {
-            numToKeep(10)
-        }
+  }
+
+  orphanedItemStrategy {
+    discardOldItems {
+      daysToKeep(7) //numToKeep(10)
     }
-}
+  }
+
+  factory {
+    workflowBranchProjectFactory {
+      scriptPath('Jenkinsfile')
+    }
+  }
+
+  configure {
+    def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
+    traits << 'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
+      strategyId(1)
+    }
+    traits << 'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+      strategyId(2)
+    }
+    traits << 'jenkins.scm.impl.trait.RegexSCMHeadFilterTrait' {
+      regex(".*(main|PR-).*")
+    }
+    traits << 'jenkins.plugins.git.traits.CleanBeforeCheckoutTrait' {
+      extension(class: 'hudson.plugins.git.extensions.impl.CleanBeforeCheckout') {
+        deleteUntrackedNestedRepositories(true)
+      }
+    }
+  }
+} 
+
