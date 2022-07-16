7a3fca82838230afae026e1e6258d686b947d7b5
Ope Omotayo <opeomotayo@gmail.com>
2022-07-16 20:38:26 +0000

    testing folder

---
+++ bld-jenkins/seed/jobs/simpleApp/folders/simpleAppFolder.groovy
@@ -0,0 +1,12 @@
+folder('simple-app-folder') {
+    displayName('simple-app-folder')
+    description('Folder for project A')
+}
+
+folder('simple-app-folder/folder1') {
+    description('Folder containing all QA jobs for project A')
+}
+
+folder('simple-app-folder/folder2') {
+    description('Folder containing all QA jobs for project A')
+}
