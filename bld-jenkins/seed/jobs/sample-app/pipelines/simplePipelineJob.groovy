e14ace96f743accf7c9a83d85dd58bf68094920e
Ope Omotayo <opeomotayo@gmail.com>
2022-07-16 15:06:56 +0000

    testing simple pipeline job

--- bld-jenkins/seed/jobs/sample-app/pipelines/simplePipelineJob.groovy
+++ bld-jenkins/seed/jobs/sample-app/pipelines/simplePipelineJob.groovy
@@ -1,7 +1,7 @@
 pipelineJob('pipelineJob') {
     definition {
         cps {
-            script(readFileFromWorkspace('simplePipeline.groovy'))
+            script(readFileFromWorkspace('../../simplePipeline.groovy'))
             sandbox()
         }
     }
