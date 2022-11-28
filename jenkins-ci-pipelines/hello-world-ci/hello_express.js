6c6d9e1931416d220a37c821542a68825c57e525
Ope Omotayo <opeomotayo@gmail.com>
2022-11-28 08:00:09 +0000

    Adding distroless helloworld

--- jenkins-ci-pipelines/hello-world-ci/hello_express.js
+++ jenkins-ci-pipelines/hello-world-ci/hello_express.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World from a distroless container'))
+app.get('/', (req, res) => res.send('<h1>Hello World from a distroless container!</h1>'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
