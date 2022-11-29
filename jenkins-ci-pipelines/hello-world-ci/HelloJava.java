acb7588144bfd59f7af50c6f4b09d9d09b7b2a73
opeomotayo <43703454+opeomotayo@users.noreply.github.com>
2022-11-29 10:25:27 +0000

    testing

--- jenkins-ci-pipelines/hello-world-ci/HelloJava.java
+++ jenkins-ci-pipelines/hello-world-ci/HelloJava.java
@@ -2,6 +2,6 @@ package examples;
 
 public class HelloJava {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Java from a distroless image");
     }
 }
